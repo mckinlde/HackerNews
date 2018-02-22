3d66693074367eb977b457fc6797316e4f7d31de
mckinled <douglas.e.mckinley@vanderbilt.edu>
2018-02-21 17:35:55 -0800

    step 1: get all the titles

---
+++ scrape_job_post_titles.py
@@ -0,0 +1,26 @@
+## This script scrapes the text of all job postings
+
+from time import sleep
+from bs4 import BeautifulSoup
+import requests
+
+
+def retrieve(url: str):
+    """retrieves content at the specified url"""
+    print("*", url)
+    header = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
+    r=requests.get(url, headers=header, verify=False, timeout=5)
+    sleep(1)
+    soup = BeautifulSoup(r.text, "lxml")
+
+    return soup
+
+def get_titles(soup: BeautifulSoup):
+    titles = []
+    for _ in soup.find_all("a", class_="storylink"):
+        titles.append(_.text)
+    return titles
+
+testSoup = retrieve('https://news.ycombinator.com/jobs')
+print(get_titles(testSoup))
+
